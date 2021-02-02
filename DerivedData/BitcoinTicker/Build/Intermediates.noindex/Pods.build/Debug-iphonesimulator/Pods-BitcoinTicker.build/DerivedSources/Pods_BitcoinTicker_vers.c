8b709fed8b181784e25271e8f871f05a1194ba56
ayodejiayankola <ayodejiayankola@gmail.com>
2021-02-02 11:56:08 +0100

    install neccessary pods

---
+++ DerivedData/BitcoinTicker/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-BitcoinTicker.build/DerivedSources/Pods_BitcoinTicker_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_BitcoinTickerVersionString[];
+ extern const double Pods_BitcoinTickerVersionNumber;
+
+ const unsigned char Pods_BitcoinTickerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_BitcoinTicker  PROJECT:Pods-1" "\n";
+ const double Pods_BitcoinTickerVersionNumber __attribute__ ((used)) = (double)1.;
